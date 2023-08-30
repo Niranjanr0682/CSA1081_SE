21c0a99feaf06f06f825158a4dce1086d38ae9eb
Niranjan Rajkumar <128575600+Niranjanr0682@users.noreply.github.com>
2023-08-30 09:31:19 +0530

    .

---
+++ Skeleton code/Admin.java
@@ -0,0 +1,107 @@
+
+
+/**
+ * Class Admin
+ */
+public class Admin {
+
+  //
+  // Fields
+  //
+
+  private int id;
+  private String Name;
+  
+  //
+  // Constructors
+  //
+  public Admin () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of id
+   * @param newVar the new value of id
+   */
+  private void setId (int newVar) {
+    id = newVar;
+  }
+
+  /**
+   * Get the value of id
+   * @return the value of id
+   */
+  private int getId () {
+    return id;
+  }
+
+  /**
+   * Set the value of Name
+   * @param newVar the new value of Name
+   */
+  private void setName (String newVar) {
+    Name = newVar;
+  }
+
+  /**
+   * Get the value of Name
+   * @return the value of Name
+   */
+  private String getName () {
+    return Name;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void ViewProducts()
+  {
+  }
+
+
+  /**
+   */
+  public void AddProducts()
+  {
+  }
+
+
+  /**
+   */
+  public void DeleteProducts()
+  {
+  }
+
+
+  /**
+   */
+  public void ModifyProduct()
+  {
+  }
+
+
+  /**
+   */
+  public void MakeProducts()
+  {
+  }
+
+
+  /**
+   */
+  public void ConfirmDelivery()
+  {
+  }
+
+
+}
---
+++ Skeleton code/Library_Management_System.java
@@ -0,0 +1,105 @@
+
+import java.util.*;
+
+
+/**
+ * Class Library_Management_System
+ */
+public class Library_Management_System {
+
+  //
+  // Fields
+  //
+
+  public String User_type;
+  public String User_name;
+  public String Password;
+  
+  //
+  // Constructors
+  //
+  public Library_Management_System () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of User_type
+   * @param newVar the new value of User_type
+   */
+  public void setUser_type (String newVar) {
+    User_type = newVar;
+  }
+
+  /**
+   * Get the value of User_type
+   * @return the value of User_type
+   */
+  public String getUser_type () {
+    return User_type;
+  }
+
+  /**
+   * Set the value of User_name
+   * @param newVar the new value of User_name
+   */
+  public void setUser_name (String newVar) {
+    User_name = newVar;
+  }
+
+  /**
+   * Get the value of User_name
+   * @return the value of User_name
+   */
+  public String getUser_name () {
+    return User_name;
+  }
+
+  /**
+   * Set the value of Password
+   * @param newVar the new value of Password
+   */
+  public void setPassword (String newVar) {
+    Password = newVar;
+  }
+
+  /**
+   * Get the value of Password
+   * @return the value of Password
+   */
+  public String getPassword () {
+    return Password;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void logic()
+  {
+  }
+
+
+  /**
+   */
+  public void register()
+  {
+  }
+
+
+  /**
+   */
+  public void logout()
+  {
+  }
+
+
+}
